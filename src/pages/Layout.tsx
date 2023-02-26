19ba08b203506001ace8c92aeaf404a4375fe637
jmorton <>
2023-02-26 22:38:32 +0000

    Add: Generic global styles. Page Description keywords & Title. Flavour text

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router";
 export default function Layout() {
   return (
     <>
-      <nav>Nav</nav>
+      <nav className="">Nav</nav>
       <Outlet />
       <footer>Footer</footer>
     </>
