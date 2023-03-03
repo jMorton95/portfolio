95d68eaab5099dac9a5d1251f0aec11a3f8d5ce9
jmorton <>
2023-03-03 22:21:31 +0000

    Add: CSS Module for /Projects, Basic rendering for Project.JSON content

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -1,6 +1,7 @@
 import projectData from '../../repository/projects.json';
 import { TProject } from '../../types/Types';
 import Project from './components/Project';
+import './projects.css';
 
 export default function Projects() {
   const projectProps: TProject[] = projectData;
--- src/pages/projects/components/Project.tsx
+++ src/pages/projects/components/Project.tsx
@@ -3,7 +3,16 @@ import { TProject } from "../../../types/Types";
 export default function Project(props: TProject) {
   return (
     <section>
-      {props.name}
+      <h3>
+        {props.name}
+      </h3>
+      <ul>
+        {props.keypoints && props.keypoints.map(point => <li>{point}</li>)}
+      </ul>
+      <p>
+        {props.description}
+      </p>
+      <a href={props.url}>Repo</a>
     </section>
   )
 };
