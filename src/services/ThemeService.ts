acdda3337b86690480841e8dd00c9852898844d5
jmorton <>
2023-03-03 16:57:25 +0000

    Refactor: Cleanup ThemeService, LINT it and change the direct localStorage reference to a reference Object

--- src/services/ThemeService.ts
+++ src/services/ThemeService.ts
@@ -1,45 +1,49 @@
 const themeOptions = {
   dark: "dark",
-  light: "light"
+  light: "light",
 };
 
-const localStorageRef = localStorage;
+const localStorageRef = window.localStorage;
 
 const metaColorScheme = document.querySelector('meta[name="color-scheme"]');
 
-const toggleTheme = (themeElement: HTMLElement, e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-  if (themeElement.classList.contains(themeOptions.dark)){
-    changeTheme(themeElement, themeOptions.light)
+const toggleTheme = (
+  themeElement: HTMLElement,
+  e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+) => {
+  if (themeElement.classList.contains(themeOptions.dark)) {
+    changeTheme(themeElement, themeOptions.light);
   } else {
     changeTheme(themeElement, themeOptions.dark);
   }
 
-  metaColorScheme?.setAttribute('content', localStorage.theme);
+  metaColorScheme?.setAttribute("content", localStorageRef.theme);
 
   setEventTargetThemeClass(e);
 };
 
-const setEventTargetThemeClass = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+const setEventTargetThemeClass = (
+  e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+) => {
   if (!(e.target instanceof Element)) return;
 
   for (let theme in themeOptions) {
-    console.log(themeOptions);
     if (e.target.classList.contains(theme)) {
       e.target.classList.remove(theme);
     }
   }
 
   e.target.classList.add(localStorageRef.theme);
-}
+};
 
 const changeTheme = (themeElement: HTMLElement, theme: string) => {
   themeElement.classList.value = theme;
-  localStorage.setItem("theme", theme);
+  localStorageRef.setItem("theme", theme);
 };
 
-const setStartupTheme = () => { 
-  document.body.classList.value = localStorage.theme;
-  metaColorScheme?.setAttribute('content', localStorage.theme);
+const setStartupTheme = () => {
+  document.body.classList.value = localStorageRef.theme;
+  metaColorScheme?.setAttribute("content", localStorageRef.theme);
 };
 
-export {toggleTheme, changeTheme, setStartupTheme}
+export { toggleTheme, changeTheme, setStartupTheme };
