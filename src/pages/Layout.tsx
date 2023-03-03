535dee0e3762700743e8e65866bf041c4c198e59
jmorton <>
2023-03-03 15:00:02 +0000

    Add: Basic theme transformation

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -2,16 +2,27 @@ import { Outlet } from "react-router";
 import { NavLink } from "react-router-dom";
 
 export default function Layout() {
+
+  function toggleTheme() {
+    const body = document.body;
+    (body.classList.contains("dark")) ? body.classList.remove("dark") : body.classList.add("dark");
+  }
+
   return (
-    <>
+    <div className="dark:bg-grey-dark dark:text-white w-screen h-screen">
       <nav className="">
-        <div>Josh M Portfolio -- Under Construction</div>
         <NavLink to="/">Projects</NavLink>
         <NavLink to="/about-me">About Me</NavLink>
+        <button type="button" onClick={toggleTheme}>Toggle Theme</button>
       </nav>
+      
       <Outlet />
-      <footer></footer>
-    </>
+
+      <footer>
+        <a href="https://github.com/jmorton95">Github</a>
+        <a href="https://github.com/jmorton95/portfolio">Source Code</a>
+      </footer>
+    </div>
     
   )
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        'grey-dark': '#1f2028'
+      }
+    },
   },
   plugins: [],
+  darkMode: 'class'
 }
