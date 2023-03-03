fd2be81663a5f967e1a54239b3b84aaab4cffbf9
jmorton <>
2023-03-03 21:18:01 +0000

    Fix: Width overflow issue

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -7,7 +7,7 @@ export default function Layout() {
     <div className="h-full bg-white transition duration-700 dark:bg-grey-dark dark:text-white">
       <div className="main-container pt-6 pb-4">
         <Nav />
-        <main className="my-auto container mx-auto">
+        <main className="container mx-auto">
           <Outlet />
         </main>
        <Footer />
