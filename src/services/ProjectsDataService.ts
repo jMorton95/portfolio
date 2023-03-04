0ac3872c9a9c5cf8ac2a1d60905cdac58d8b860b
jmorton <>
2023-03-04 00:06:05 +0000

    Comment: JSDoc for ProjectsDataService & tidy-up

--- src/services/ProjectsDataService.ts
+++ src/services/ProjectsDataService.ts
@@ -1,10 +1,13 @@
-// export function checkLogoIsValid(value: string): value is ETechLogosPaths {
-//   return Object.values<string>(ETechLogosPaths).includes(value);
-// }
-
+/**
+ * @param enumObj The Enum Type you wish to validate against
+ * @param arr The Array you wish to compare to an Emum
+ * @returns An Array of strings filtered against your Enum
+ */
 export function filterArrayAgainstEnum<T extends Record<string, string>>(
   enumObj: T,
   arr: readonly string[]
 ): T[keyof T][] {
-  return arr.filter((value) => Object.values(enumObj).includes(value)) as T[keyof T][];
+  return arr.filter((value) =>
+    Object.values(enumObj).includes(value)
+  ) as T[keyof T][];
 }
