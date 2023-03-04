5c24f51088112d0370760f0760b090b7ddf1d15e
jmorton <>
2023-03-04 00:04:06 +0000

    Chore: Clean up logging & add some missing {keys} in mapped arrays.

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -5,7 +5,6 @@ import './projects.css';
 
 export default function Projects() {
   const projectProps: TProject[] = projectData;
-  console.log(projectProps);
   return (
     <div className="projects">
     {projectProps.length > 0 && projectProps.map(project => <Project key={project.id} {...project}/>)}
--- src/pages/projects/components/Project.tsx
+++ src/pages/projects/components/Project.tsx
@@ -2,20 +2,20 @@ import { filterArrayAgainstEnum } from "../../../services/ProjectsDataService";
 import { ETechLogosPaths, TProject } from "../../../types/Types";
 
 export default function Project(props: TProject) {
-  const validLogos = filterArrayAgainstEnum(ETechLogosPaths, props.logos );
+  const validLogos = filterArrayAgainstEnum(ETechLogosPaths, props.logos);
   return (
     <section className="py-6">
       <h3>
         {props.name}
       </h3>
       <ul>
-        {props.keypoints && props.keypoints.map(point => <li>{point}</li>)}
+        {props.keypoints && props.keypoints.map((point, index) => <li key={index}>{point}</li>)}
       </ul>
       <p>
         {props.description}
       </p>
       <a href={props.url}>Repo</a>
-      {validLogos && validLogos.map(logo => <p>{logo}</p>)}
+      {validLogos && validLogos.map((logo, index) => <p key={index}>{logo}</p>)}
     </section>
   )
 };
